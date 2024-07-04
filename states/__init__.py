653461870efac5e0849462022fbc96e03d9ebec9
Krishpy Chips <krishpychips@gmail.com>
2024-07-04 23:41:49 +0400

    Added GAME_STATES attribute

--- states/__init__.py
+++ states/__init__.py
@@ -1,4 +1,8 @@
 from states.base import State, StateManager
+from states.game import Game
+from states.menu import Menu
+from states.settings import Settings
 from typing import Tuple
 
 __all__: Tuple[str, ...] = ("State", "StateManager")
+GAME_STATES: Tuple[State, ...] = (Game, Menu, Settings)
