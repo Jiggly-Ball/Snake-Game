7de7da0050803e8e1842599dfbef25181e6d9fe9
Krishpy Chips <krishpychips@gmail.com>
2024-07-05 22:10:23 +0400

    Added button & text presets

---
+++ core/preset.py
@@ -0,0 +1,6 @@
+from core.utils import ButtonStyle, TextStyle
+
+
+blue_text_style = TextStyle(0xFFFF14)
+red_text_style = TextStyle("red")
+menu_button_style = ButtonStyle(150, 40, blue_text_style)
