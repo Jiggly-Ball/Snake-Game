b1a08c45254b648e4bf514e82f5adec0aae8249b
Krishpy Chips <krishpychips@gmail.com>
2024-07-05 23:57:33 +0400

    Revised game var positions

--- states/game.py
+++ states/game.py
@@ -32,6 +32,7 @@ class Game(State):
         )
 
     def run(self) -> None:
+
         snake = Snake()
         apple = Apple.spawn(snake=snake)
         self.bg_music.set_volume(self.volume.bg_vol)
@@ -39,10 +40,10 @@ class Game(State):
         self.eat_sound.set_volume(self.volume.sfx_vol)
         self.bg_music.play(-1)
 
-        self.death_play = False
-        self.move = False  # For handling rapid key strokes; allows only a single keystroke every frame.
-        self.fps = BASE_FPS
-        self.highscore = load_highscore(HIGHSCORE_PATH)
+        death_play = False
+        move = False  # For handling rapid key strokes; allows only a single keystroke every frame.
+        fps = BASE_FPS
+        highscore = load_highscore(HIGHSCORE_PATH)
 
         while True:
             self.window.fill(
@@ -58,32 +59,32 @@ class Game(State):
                     raise ExitGameError()
 
                 if event.type == KEYDOWN:
-                    if not self.move:
+                    if not move:
                         if snake.x_dir != 1 and (event.key in KEY_LEFT):
                             snake.x_dir = snake.x_dir or -1
                             snake.y_dir = 0
-                            self.move = True
+                            move = True
                         elif snake.x_dir != -1 and (event.key in KEY_RIGHT):
                             snake.x_dir = snake.x_dir or 1
                             snake.y_dir = 0
-                            self.move = True
+                            move = True
                         elif snake.y_dir != -1 and (event.key in KEY_UP):
                             snake.x_dir = 0
                             snake.y_dir = snake.y_dir or -1
-                            self.move = True
+                            move = True
                         elif snake.y_dir != 1 and (event.key in KEY_DOWN):
                             snake.x_dir = 0
                             snake.y_dir = snake.y_dir or 1
-                            self.move = True
+                            move = True
 
                 if event.type == MOUSEBUTTONDOWN and snake.dead:
                     self.manager.change_state("Menu")
                     self.manager.exit_current_state()
                     # snake = Snake()
                     # apple = Apple.spawn(snake=snake)
-                    # self.fps = BASE_FPS
+                    # fps = BASE_FPS
                     # self.bg_music.play(-1)
-                    # self.death_play = False
+                    # death_play = False
 
             if snake.head.x == apple.x and snake.head.y == apple.y:
                 self.eat_sound.play()
@@ -93,33 +94,33 @@ class Game(State):
                 )
                 apple = Apple.spawn(snake=snake)
                 if len(snake.body) % 5 == 0:
-                    self.fps += 2
+                    fps += 2
 
             apple.update(self.window)
             snake.update()
             draw_grid(self.window)
 
             pygame.draw.rect(self.window, SNAKE_COLOUR, snake.head, BLOCK_SIZE)
-            self.move = False
+            move = False
             for body in snake.body:
                 pygame.draw.rect(self.window, SNAKE_COLOUR, body, BLOCK_SIZE)
 
             self.score_text.render(f"{len(snake.body)}")
-            self.highscore_text.render(f"HIGHSCORE: {self.highscore}")
+            self.highscore_text.render(f"HIGHSCORE: {highscore}")
 
             if snake.dead:
                 self.bg_music.stop()
-                if not self.death_play:
+                if not death_play:
                     self.death_sound.play()
-                    self.death_play = True
+                    death_play = True
 
                 self.death_text.render("You have died!")
                 self.restart_text.render("Click anywhere on the screen to restart.")
 
-                if len(snake.body) > self.highscore:
-                    self.highscore = len(snake.body)
-                    save_highscore(HIGHSCORE_PATH, self.highscore)
-                    self.highscore_text.render(f"HIGHSCORE: {self.highscore}")
+                if len(snake.body) > highscore:
+                    highscore = len(snake.body)
+                    save_highscore(HIGHSCORE_PATH, highscore)
+                    self.highscore_text.render(f"HIGHSCORE: {highscore}")
 
             pygame.display.update()
-            self.clock.tick(self.fps)
+            self.clock.tick(fps)
