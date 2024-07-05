ddc973a1f0ed09633a3ebbe2ea5ce2c2b99b7e26
Krishpy Chips <krishpychips@gmail.com>
2024-07-05 22:15:32 +0400

    Added volume attr to State & updated StateManager

--- states/base.py
+++ states/base.py
@@ -3,13 +3,16 @@ from __future__ import annotations
 import pygame
 from typing import Dict, Optional, Generator
 
-from core.utils import Text
-from core.const import MENU_COLOUR, BUTTON_WIDTH, BUTTON_HEIGHT, BUTTON_TEXT_SIZE
+from core.errors import StateError, ExitStateError
+from core.utils import MusicVol
 
 
 class State:
-    def __init__(self, *, window: pygame.Surface, manager: StateManager) -> None:
+    def __init__(
+        self, /, window: pygame.Surface, volume: MusicVol, manager: StateManager
+    ) -> None:
         self.window = window
+        self.volume = volume
         self.manager = manager
         self.clock = pygame.time.Clock()
 
@@ -18,30 +21,42 @@ class State:
 
 
 class StateManager:
-    __slots__ = ("__states", "current_state")
+    __slots__ = ("__states", "__current_state")
 
-    def __init__(self, window: pygame.Surface, *states: State) -> None:
+    def __init__(
+        self, window: pygame.Surface, volume: MusicVol, *states: State
+    ) -> None:
         for state in states:
             assert issubclass(
                 state, State
-            ), f"Expected subclass of `{State}` instead got `{state.__mro__[-2]}`"
+            ), f"Expected subclass of `{State}` instead got `{state.__mro__[-2]}`."
 
         self.__states: Dict[str, State] = {
-            class_.__name__: class_(window=window, manager=self) for class_ in states
+            class_.__name__: class_(window, volume, self) for class_ in states
         }
-        self.current_state: Optional[State] = None
+        self.__current_state: Optional[State] = None
 
     def change_state(self, state_name: str) -> None:
         assert (
             state_name in self.__states
-        ), f"State `{state_name}` isn't present from the available states: `{', '.join(self.get_all_states())}`"
-        self.current_state = self.__states[state_name]
+        ), f"State `{state_name}` isn't present from the available states: `{', '.join(self.get_all_states())}`."
+        self.__current_state = self.__states[state_name]
 
     def get_state(self) -> Optional[State]:
-        return self.current_state
+        return self.__current_state
 
     def get_all_states(self) -> Generator[str]:
         return (state for state in self.__states.keys())
 
     def run_current_state(self) -> None:
-        self.current_state.run()
+        if self.__current_state is not None:
+            self.__current_state.run()
+        else:
+            raise StateError("No state has been set to run.")
+
+    def exit_current_state(self) -> None:
+        if self.__current_state is not None:
+            self.__current_state.button_enable = False
+            raise ExitStateError()
+        else:
+            raise StateError("No state has been set to exit from.")
